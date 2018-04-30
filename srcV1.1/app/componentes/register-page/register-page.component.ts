2ea6921a09c18966f1d2c60b2a4311a802c95f2d
GsDugot <38463001+GsDugot@users.noreply.github.com>
2018-04-30 08:26:34 -0400

    Conexion Firebase

---
+++ srcV1.1/app/componentes/register-page/register-page.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../servicios/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.scss']
+})
+export class RegisterPageComponent implements OnInit {
+
+public email: string;
+public password: string;
+
+  constructor(public authService: AuthService, public router: Router) 
+  {
+  }
+  ngOnInit() 
+  {
+  }
+
+  //verifica en consola si el usuario se registra correctamente
+  onSubmitAddUser()
+  {
+    this.authService.registerUser(this.email, this.password)
+    .then((res) => {this.router.navigate(['/privado']);
+  }).catch((err) => {
+    console.log(err);
+  });
+  }
+}
---
+++ srcV1.1/app/modelos/estudiante.ts
@@ -0,0 +1,23 @@
+export class Estudiante {
+
+    nombre: string;
+    apellido: string;
+    codigo: number;
+    email: string;
+    celular: number;
+    username: string;
+    password: string;
+
+    constructor(nombre: string, apellido: string, codigo: number,
+        email: string, celular: number, username: string,
+        password: string ) {
+
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.codigo = codigo;
+            this.email = email;
+            this.celular = celular;
+            this.username = username;
+            this.password = password;
+    }
+}
