7084a50646f649ab294014daf8f7cecf2d87751b
GsDugot <38463001+GsDugot@users.noreply.github.com>
2018-05-10 11:20:55 -0400

    Add files via upload
    validacion nickname libre

---
+++ srcV1.2/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig:
+  {
+    apiKey: "AIzaSyCd73spCngujttVcS9Dga6akanUBHe89vo",
+    authDomain: "liftapp-f892f.firebaseapp.com",
+    databaseURL: "https://liftapp-f892f.firebaseio.com",
+    projectId: "liftapp-f892f",
+    storageBucket: "liftapp-f892f.appspot.com",
+    messagingSenderId: "870835801913"
+  }
+};
